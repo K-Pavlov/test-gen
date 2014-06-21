f143d790c08fac254749d8c389f2941e20f09043
Kaloyan <kaloian.pavlov@abv.bg>
2014-06-21 17:58:34 +0300

    Random number generation

---
+++ test-gen/src/scripts/random-generation.js
@@ -0,0 +1,73 @@
+﻿function generateHardNumber(digitCount) {
+    var number = "0x";
+    for (var i = 0; i < digitCount; i++) {
+        number += generateRandomDigit();
+    }
+
+    return number;
+}
+
+function generateRandomDigit() {
+    var randomDigit = Math.floor((Math.random() * 16));
+    var correctDigit = getCorretDigit(randomDigit);
+    return correctDigit;
+}
+
+function getCorretDigit(digit) {
+    if (digit < 10) {
+        return digit;
+    }
+
+    switch (digit) {
+        case 10:
+            return 'A';
+        case 11:
+            return 'B';
+        case 12:
+            return 'C';
+        case 13:
+            return 'D';
+        case 14:
+            return 'E';
+        case 15:
+            return 'F';
+    }
+
+    return digit;
+}
+
+function generateMediumNumber(digitCount) {
+    var number = '0x';
+    // if it's 0 the number will go like 0F0F
+    // if it's 1 like F0F0
+    var decision = Math.floor(Math.random() * 2);
+    if (decision) {
+        for (var i = 0; i < digitCount; i++) {
+            if (i % 2 === 0) {
+                number += '0';
+            } else {
+                number += generateRandomDigit();
+            }
+        }
+    } else {
+        for (var i = 0; i < digitCount; i++) {
+            if (i % 2 !== 0) {
+                number += '0';
+            } else {
+                number += generateRandomDigit();
+            }
+        }
+    }
+
+    return number;
+}
+
+function generateEasyNumber() {
+    var number = '0x0';
+    var randomDigit;
+    while (!(randomDigit = generateRandomDigit()));
+
+    number += randomDigit + '0' + randomDigit;
+
+    return number;
+}
---
+++ test-gen/src/scripts/simple-test-gen.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jQuery1.11.1.js" />
+/// <reference path="handlebars-v1.3.0.js" />
+/// <reference path="random-generation.js" />
+function easyTaskOneTwo() {
+    var templateNode = document.getElementById('template');
+    var templateString = templateNode.innerHTML;
+
+    var template = Handlebars.compile(templateString);
+
+    var valueVariable = new variableValuePair('int i', generateEasyNumber(4));
+    console.log(valueVariable);
+
+    var taskOneHtml = template(valueVariable);
+    document.body.innerHTML = taskOneHtml;
+    console.log(taskOneHtml);
+}
+
+function variableValuePair(variable, value) {
+    this.variable = variable;
+    this.value = value;
+}
+
+easyTaskOneTwo();
